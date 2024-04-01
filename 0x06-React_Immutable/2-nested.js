35a8f810ba15d0a991dd2edea17e13c1ce953071
otunba32 <emmanuel.abia32@gmail.com>
2024-04-01 08:06:51 -0400

    Accessing nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,9 @@
+import { fromJS } from 'immutable'
+
+export default function accessImmutableObject(object, array) {
+    try {
+        return fromJS(object).getIn(array);
+    } catch (error) {
+        return undefined;
+    }
+}
